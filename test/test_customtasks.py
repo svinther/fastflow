5d71fdf15f19f45ec57a3ec9f46e8367a241c6ed
Steffen Vinther Sørensen <svinther@gmail.com>
2024-10-29 15:36:59 +0100

    Unit tests for custom tasks. Add custom task and test for Sleep task
    - Added `Sleep` task implementation in `test/customtasks/testtasks.py`.
    - Created a test for the `Sleep` task in `test/test_customtasks.py`.
    - The `Sleep` task logs a message and sleeps for the specified duration.
    - The test verifies the workflow completes successfully.

---
+++ test/test_customtasks.py
@@ -0,0 +1,48 @@
+import base64
+import pathlib
+from time import sleep
+from typing import Optional
+
+from testutil import (
+    OPERATOR_NAMESPACE,
+    AbstractOperatorTest,
+    get_cr,
+    get_k8s_task_object,
+)
+
+from fastflow.helpers import create_workflow_crd_object
+from fastflow.models import WORKFLOWSTATUS, WorkflowCRD
+
+
+# language=yaml
+dag_yaml = """\
+- name: testcustomtasks
+  impl: testtasks.Sleep
+  inputs:
+    howlong: 1
+"""
+
+WFNAME = "testcustomtasks"
+
+workflow = create_workflow_crd_object(
+    WFNAME,
+    dag_yaml,
+    labels={"customer_nam": "dummy"},
+)
+
+
+def test_it():
+    with AbstractOperatorTest(workflow, paths=[str(pathlib.Path(__file__).parents[0] / "customtasks" / "testtasks.py")]) as _:
+        while True:
+            sleep(2)
+            wf = get_cr(WFNAME, OPERATOR_NAMESPACE, WorkflowCRD)
+            wf_status_str = wf.get("status", {}).get("workflow_status", None)
+            if wf_status_str:
+                wf_status = WORKFLOWSTATUS(wf_status_str)
+                if wf_status in (
+                    WORKFLOWSTATUS.complete,
+                    WORKFLOWSTATUS.failed,
+                ):
+                    break
+
+        assert wf_status == WORKFLOWSTATUS.complete
